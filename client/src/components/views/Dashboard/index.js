f0604991fcb36c85fbc7f95b392d5359be708bd2
ilhan69 <68482895+ilhan69@users.noreply.github.com>
2022-10-12 16:34:46 +0200

    Update index.js

--- client/src/components/views/Dashboard/index.js
+++ client/src/components/views/Dashboard/index.js
@@ -1,9 +1,9 @@
 import axios from 'axios'
 import { useEffect, useState } from "react"
-import { Col, Row, Spinner, Modal, Button, Image } from "react-bootstrap"
+import { Button, Card, Col, Image, Modal, Row, Spinner } from "react-bootstrap"
 import DashboardLayout from "../../layouts/DashboardLayout"
 
-import { Input } from 'reactstrap'
+import { CardBody, Input } from 'reactstrap'
 import { API_URL_REALISATIONS } from '../../../config/constants'
 
 const Dashboard = () => {
@@ -23,18 +23,22 @@ const Dashboard = () => {
             setFilteredData(res.data.data)
         })
     }, [])
-
     return (
         <DashboardLayout title='Accueil'>
-            <p className='h3'>Découvrez les réalisations</p>
+            <p className='h3'>Découvrez des réalisations de la taverne du design</p>
 
             {dataList !== null ? (
                 <>
                     <Input type='text' onChange={handleFilter} placeholder='Checher dans les réalisations...' />
+                    <hr className='m-5' />
                     <Row>
                         {filteredData.map((d, i) => {
-                            return (<Col key={i} md={3}>
-                                <span onClick={() => setRealisationShowing(d)}>{d.name}</span>
+                            return (<Col onClick={() => setRealisationShowing(d)} className='p-1' key={i} md={3}>
+                                <Card>
+                                    <CardBody>
+                                        <span>{d.name}</span>
+                                    </CardBody>
+                                </Card>
                             </Col>)
                         })}
                     </Row>
@@ -50,7 +54,19 @@ const Dashboard = () => {
                         <Modal.Title>{realisationShowing.name}</Modal.Title>
                     </Modal.Header>
                     <Modal.Body>
-                        {realisationShowing.files.map((image, i) => <Image key={i} className='mw-100 image-fluid' src={image.file_url} />)}
+                        {realisationShowing.files.map((media, i) => {
+                            if (media.file_type === 'VIDEO') {
+                                return <video key={i} controls className='mw-100 image-fluid'>
+                                    <source src={media.file_url}
+                                        type="video/mp4" /></video>
+                            } else if (media.file_type === 'YOUTUBE') {
+                                return <iframe key={i} className='mw-100 image-fluid' title={media.name} src={media.file_url} frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
+                            } else if (media.file_type === 'IMAGE') {
+                                return <Image key={i} className='mw-100 image-fluid' src={media.file_url} />
+                            } else {
+                                return <></>
+                            }
+                        })}
                     </Modal.Body>
                     <Modal.Footer>
                         <Button variant="secondary" onClick={() => setRealisationShowing(null)}>
